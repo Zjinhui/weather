0f398a1ab8eb2f7cda4d6fad532242ad372cfe1f
Zjinhui <1194226964@qq.com>
2019-04-15 16:55:16 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/huiview/weather/activity/WeatherActivity.java
+++ app/src/main/java/com/huiview/weather/activity/WeatherActivity.java
@@ -15,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.huiview.weather.R;
+import com.huiview.weather.service.AutoUpdateService;
 import com.huiview.weather.util.HttpCallbackListener;
 import com.huiview.weather.util.HttpUtil;
 import com.huiview.weather.util.Utility;
@@ -166,9 +167,12 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         temp1Text.setText(prefs.getString("temp1",""));
         temp2Text.setText(prefs.getString("temp2",""));
         weatherDespText.setText(prefs.getString("weather_desp",""));
-        publishText.setText("今天"+prefs.getString("publish_time","")+"发布");
+        publishText.setText("昨天"+prefs.getString("publish_time","")+"发布");
         currentDateText.setText(prefs.getString("current_date",""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
     }
 }
